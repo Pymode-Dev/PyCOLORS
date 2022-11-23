fbac0749cd471bc73855fd7d1a45e6a1239d9358
Pymode-Dev <pymode000@gmail.com>
2022-11-23 16:30:59 +0100

    My First Push

---
+++ colors.py
@@ -0,0 +1,9 @@
+class Colors:
+    """Class For the COLORS"""
+    BLACK = '#121212'
+    GRAY = '#686868'
+    ORANGE = 'orange'
+    WHITE = '#ffffff'
+    LIGHT_BLUE = '#5799ef'
+    LIST_ODD_LINE = '#f6f6f5'
+    LIST_EVEN_LINE = '#fbfbfb'
